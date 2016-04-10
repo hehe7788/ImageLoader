1008e517f11cab3e55030c3a8eb15af81ecd679a
hehe7788 <hehe7788ycc@gmail.com>
2016-04-10 23:47:04 +0800

    Gridview布局

--- imageloader/src/main/java/com/ycc/imageloader/MainActivity.java
+++ imageloader/src/main/java/com/ycc/imageloader/MainActivity.java
@@ -1,16 +1,175 @@
 package com.ycc.imageloader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.media.MediaActionSound;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.ycc.imageloader.bean.FolderBean;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends Activity {
+
+    private GridView mGridView;
+    private List<String> mImages;
+
+    private RelativeLayout mBottomBar;
+    private TextView mDirName;
+    private TextView mDirCount;
+
+    private ProgressDialog mProgressDialog;
+    private File mCurrentDir;
+    private int mMaxCount;
+
+    private List<FolderBean> mFolderBeans = new ArrayList<>();
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0x111) {
+                mProgressDialog.dismiss();
+                //绑定数据到view中
+                dataToView();
+            }
+
+        }
+    };
+
+    protected void dataToView() {
+        if (mCurrentDir == null) {
+            Toast.makeText(this, "未扫描到任何图片", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //数组转为List
+        mImages = Arrays.asList(mCurrentDir.list());
+        //使用mImages构造gridview的适配器
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        loadData();
+        initEvent();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+        mGridView = (GridView) findViewById(R.id.main_gridView);
+        mBottomBar = (RelativeLayout) findViewById(R.id.main_bottom_bar);
+        mDirName = (TextView) findViewById(R.id.main_dir_name);
+        mDirCount = (TextView) findViewById(R.id.main_dir_count);
+    }
+
+    /**
+     *开启线程，利用content provider，扫描手机中的图片，通过handler通知主线程更新ui
+     */
+    private void loadData() {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(this, "当前存储卡不可用", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mProgressDialog = ProgressDialog.show(this, null, "loading");
+
+        new Thread() {
+            @Override
+            public void run() {
+                Uri mImgUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                ContentResolver cr = MainActivity.this.getContentResolver();
+
+                Cursor cursor = cr.query(mImgUri, null, MediaStore.Images.Media.MIME_TYPE
+                                + " =?or" + MediaStore.Images.Media.MIME_TYPE
+                                + " =?", new String[]{"image/jpeg", "image/png"},
+                        MediaStore.Images.Media.DATE_MODIFIED);
+
+                Set<String> mDirPaths = new HashSet<>();
+
+                if (cursor != null) {
+                    while (cursor.moveToNext()) {
+                        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                        File parentFile = new File(path).getParentFile();
+
+                        if (parentFile == null) {
+                            continue;
+                        }
+                        String dirPath = parentFile.getAbsolutePath();
+
+                        FolderBean folderBean;
+
+                        //防止重复扫描
+                        if (mDirPaths.contains(dirPath)) {
+                            continue;
+                        } else {
+                            mDirPaths.add(dirPath);
+                            folderBean = new FolderBean();
+                            folderBean.setDir(dirPath);
+                            folderBean.setFirstImgPath(path);
+
+                        }
+
+                        if (parentFile.list() == null) {
+                            continue;
+                        }
+
+                        int picSize = parentFile.list(new FilenameFilter() {
+                            @Override
+                            public boolean accept(File dir, String filename) {
+                                return filename.endsWith(".jpg")
+                                        || filename.endsWith(".jpeg")
+                                        || filename.endsWith(".png");
+                            }
+                        }).length;
+
+                        folderBean.setCount(picSize);
+                        mFolderBeans.add(folderBean);
+
+                        if (picSize > mMaxCount) {
+                            mMaxCount = picSize;
+                            mCurrentDir = parentFile;
+                        }
+
+
+
+                    }
+                    cursor.close();
+                    //通知handler图片扫描完成
+                    mHandler.sendEmptyMessage(0x111);
+                }
+            }
+        }.start();
+    }
+
+    /**
+     * 控件的点击事件
+     */
+    private void initEvent() {
+
     }
 
     @Override
